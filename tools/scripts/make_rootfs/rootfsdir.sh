157dfa4532a755fa136409f4d882bac64bfcf26a
pilipala195 <yangguangzhao1@huawei.com>
2021-03-25 11:11:15 +0800

    Refactoring the packaging process, copy bin and lib to out path

--- tools/scripts/make_rootfs/rootfsdir.sh
+++ tools/scripts/make_rootfs/rootfsdir.sh
@@ -33,10 +33,18 @@ set -e
 BIN_DIR=$1
 LIB_DIR=$2
 ROOTFS_DIR=$3
+OUT_DIR=$4
 mkdir -p ${ROOTFS_DIR}/bin ${ROOTFS_DIR}/lib ${ROOTFS_DIR}/usr/bin ${ROOTFS_DIR}/usr/lib ${ROOTFS_DIR}/etc \
 ${ROOTFS_DIR}/app ${ROOTFS_DIR}/data ${ROOTFS_DIR}/proc ${ROOTFS_DIR}/dev ${ROOTFS_DIR}/data/system ${ROOTFS_DIR}/data/system/param \
 ${ROOTFS_DIR}/system ${ROOTFS_DIR}/system/internal ${ROOTFS_DIR}/system/external
 if [ -d "${BIN_DIR}" ] && [ "$(ls -A "${BIN_DIR}")" != "" ]; then
     cp -f ${BIN_DIR}/* ${ROOTFS_DIR}/bin
+    if [ -e ${OUT_DIR}/bin/shell ]; then
+        cp -f ${BIN_DIR}/shell ${OUT_DIR}/bin/shell
+    fi
+    if [ -e ${OUT_DIR}/bin/tftp ]; then
+        cp -f ${BIN_DIR}/tftp ${OUT_DIR}/bin/tftp
+    fi
 fi
 cp -f ${LIB_DIR}/* ${ROOTFS_DIR}/lib
+cp -f ${LIB_DIR}/* ${OUT_DIR}/libs
