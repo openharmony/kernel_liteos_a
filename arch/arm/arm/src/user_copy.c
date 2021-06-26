9db3407589bb0b2d4a5772faac3130032bc0b8a9
Haryslee <lihao189@huawei.com>
2021-06-26 14:36:00 +0800

    feat: 基于汇编实现内核对用户态内存清零的功能
    背景：LOS_UserMemClear接口原有实现是通过在内核中
    申请一块堆内存并对其清零，调用copy_to_user来达到
    对用户态内存清零的目的，需要使用堆内存。
    修改方案：基于汇编实现内核对用户态内存清零的功能。
    
    close #I3XXT0
    
    Change-Id: I27cb1e45559cb75a9b330799fe427abd54f51c15
    Signed-off-by: Haryslee <lihao189@huawei.com>

--- arch/arm/arm/src/user_copy.c
+++ arch/arm/arm/src/user_copy.c
@@ -31,6 +31,7 @@
 
 #include "user_copy.h"
 #include "arm_user_copy.h"
+#include "arm_user_clear.h"
 #include "securec.h"
 #include "los_memory.h"
 #include "los_vm_map.h"
@@ -96,15 +97,9 @@ INT32 LOS_UserMemClear(unsigned char *buf, UINT32 len)
     if (!LOS_IsUserAddressRange((vaddr_t)(UINTPTR)buf, len)) {
         (VOID)memset_s(buf, len, 0, len);
     } else {
-        unsigned char *tmp = (unsigned char *)LOS_MemAlloc(OS_SYS_MEM_ADDR, len);
-        if (tmp == NULL) {
-            return -ENOMEM;
+        if (_arm_clear_user(buf, len)) {
+            return -EFAULT;
         }
-        (VOID)memset_s(tmp, len, 0, len);
-        if (_arm_user_copy(buf, tmp, len) != 0) {
-            ret = -EFAULT;
-        }
-        LOS_MemFree(OS_SYS_MEM_ADDR, tmp);
     }
     return ret;
 }
