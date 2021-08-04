ed7defbd439c345a07159d40eb04433afd530004
lnlan <lanleinan@163.com>
2021-08-04 00:56:39 +0000

    fix: 修复sigwait等待到的信号值与获取的siginfo中的值不一致 【背景】 集成测试发送两个不同的信号，sigwait第二次等到的仍是第一个信号 经定位，信号在kill时会将相关的siginfo信息拷贝到taskcb的unbinfo中，sigwait 处理时从unbinfo拷贝给用户。若此信号发送时处于屏蔽状态，再有其他信号发送会覆盖 掉unbinfo，此时sigwait等待这个信号获取到的info已经被覆盖 【修改方案】 1. 每个任务添加一个siginfo缓存链表，在处理信号前夕从缓存链表取出info到unbinfo中
    【影响】
    对现有的产品编译不会有影响。
    
    re #I3M12H
    
    Signed-off-by: lanleinan <lanleinan@163.com>
    
    Change-Id: If4b064c18773f8eca7419c665977260167b09810

--- kernel/base/core/los_task.c
+++ kernel/base/core/los_task.c
@@ -498,6 +498,7 @@ LITE_OS_SEC_TEXT VOID OsTaskResourcesToFree(LosTaskCB *taskCB)
         OsTaskKernelResourcesToFree(syncSignal, topOfStack);
 
         SCHEDULER_LOCK(intSave);
+        OsClearSigInfoTmpList(&(taskCB->sig));
         OsInsertTCBToFreeList(taskCB);
         SCHEDULER_UNLOCK(intSave);
     }
