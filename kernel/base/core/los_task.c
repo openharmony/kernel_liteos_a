64e49aba7c9c7d2280c5b3f29f04b17b63209855
zhushengle <zhushengle@huawei.com>
2021-11-29 11:05:52 +0800

    feat: 支持L1 低功耗框架
    方案描述：
       和L0保持一致，上层通过proc文件系统操作：
       power_mode  支持的低功耗模式，通过对该文件进行write操作可以设置低功耗模式
       power_count powermanager模块通过对该文件操作，和内核进行交互，简要流程如下：
         while (1) {
           open  // 打开该文件
           read  // 使powermanager低功耗任务常阻塞，当系统无任何模块持锁时，会唤醒该任务
           write // 进行低功耗流程
           close // 关闭该文件
         }
       power_lock    write该文件，持锁
       power_unlock  writw该文件，释放锁
    
    Close #I4JSO
    
    Change-Id: I73fcdeeb5e2039484b3351a81b46a0892b349fe9
    Signed-off-by: zhushengle <zhushengle@huawei.com>

--- kernel/base/core/los_task.c
+++ kernel/base/core/los_task.c
@@ -78,10 +78,6 @@ LITE_OS_SEC_BSS SPIN_LOCK_INIT(g_taskSpin);
 STATIC VOID OsConsoleIDSetHook(UINT32 param1,
                                UINT32 param2) __attribute__((weakref("OsSetConsoleID")));
 
-#define OS_CHECK_TASK_BLOCK (OS_TASK_STATUS_DELAY |    \
-                             OS_TASK_STATUS_PENDING |  \
-                             OS_TASK_STATUS_SUSPENDED)
-
 /* temp task blocks for booting procedure */
 LITE_OS_SEC_BSS STATIC LosTaskCB                g_mainTask[LOSCFG_KERNEL_CORE_NUM];
 
@@ -736,7 +732,6 @@ LITE_OS_SEC_TEXT_INIT UINT32 LOS_TaskResume(UINT32 taskID)
     UINT32 intSave;
     UINT32 errRet;
     LosTaskCB *taskCB = NULL;
-    BOOL needSched = FALSE;
 
     if (OS_TID_CHECK_INVALID(taskID)) {
         return LOS_ERRNO_TSK_ID_INVALID;
@@ -756,17 +751,11 @@ LITE_OS_SEC_TEXT_INIT UINT32 LOS_TaskResume(UINT32 taskID)
         OS_GOTO_ERREND();
     }
 
-    taskCB->taskStatus &= ~OS_TASK_STATUS_SUSPENDED;
-    if (!(taskCB->taskStatus & OS_CHECK_TASK_BLOCK)) {
-        OsSchedTaskEnQueue(taskCB);
-        if (OS_SCHEDULER_ACTIVE) {
-            needSched = TRUE;
-        }
-    }
+    BOOL needSched = OsSchedResume(taskCB);
     SCHEDULER_UNLOCK(intSave);
 
     LOS_MpSchedule(OS_MP_CPU_ALL);
-    if (needSched) {
+    if (OS_SCHEDULER_ACTIVE && needSched) {
         LOS_Schedule();
     }
 
@@ -834,16 +823,7 @@ LITE_OS_SEC_TEXT STATIC UINT32 OsTaskSuspend(LosTaskCB *taskCB)
         return errRet;
     }
 
-    if (tempStatus & OS_TASK_STATUS_READY) {
-        OsSchedTaskDeQueue(taskCB);
-    }
-
-    taskCB->taskStatus |= OS_TASK_STATUS_SUSPENDED;
-    OsHookCall(LOS_HOOK_TYPE_MOVEDTASKTOSUSPENDEDLIST, taskCB);
-    if (taskCB == OsCurrTaskGet()) {
-        OsSchedResched();
-    }
-
+    OsSchedSuspend(taskCB);
     return LOS_OK;
 }
 
