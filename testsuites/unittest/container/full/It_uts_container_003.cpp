347e24f6805797233c43860a4fdf6d2eaee76bb2
zhushengle <zhushengle@huawei.com>
2023-03-09 13:48:12 +0800

    fix: 修复网络容器测试失败问题
    1.修复网络容器测试失败问题
    2.调整测试用例之间的顺序，提高测试用例的稳定性
    3.极限压测用例增加延时，等待系统资源回收，增加用例稳定性
    4.优化网络容器测试用例重复代码
    
    Close #I6L7VF
    Signed-off-by: zhushengle <zhushengle@huawei.com>
    Change-Id: If800793430c872c432f5fdf2d9c2d968fafb3973

--- testsuites/unittest/container/full/It_uts_container_003.cpp
+++ testsuites/unittest/container/full/It_uts_container_003.cpp
@@ -78,4 +78,6 @@ void ItUtsContainer003(void)
     ASSERT_EQ(ret, pid);
     status = WEXITSTATUS(status);
     ASSERT_EQ(status, EXIT_CODE_ERRNO_5);
+
+    sleep(5); /* 5: Wait for process resources to be reclaimed */
 }
