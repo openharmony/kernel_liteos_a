dd7e62be63f59d36dfc30d1e37f92207e5eee6b1
hw_llm <liu.limin@huawei.com>
2024-09-20 16:42:19 +0800

    Description: VM Map 添加LiteIPC 保护 IssueNo: https://gitee.com/openharmony/kernel_liteos_a/issues/IASJLL Feature Or Bugfix: Bugfix Binary Source: No Signed-off-by: hw_llm <liu.limin@huawei.com> （cherry picked commit from <gitee.com//openharmony/kernel_liteos_a/commit/75af9e28d9e92398f71e2f884c53a2745e7cd097>

--- kernel/base/vm/los_vm_dump.c
+++ kernel/base/vm/los_vm_dump.c
@@ -76,6 +76,8 @@ const CHAR *OsGetRegionNameOrFilePath(LosVmMapRegion *region)
         return "MMAP";
     } else if (region->regionFlags & VM_MAP_REGION_FLAG_SHM) {
         return "SHM";
+    } else if (region->regionFlags & VM_MAP_REGION_FLAG_LITEIPC) {
+        return "LITEIPC";
     } else {
         return "";
     }
